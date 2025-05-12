758794302243c5a69158dddfaff5d3a3d9a379ff
Kobkit Ruangsuriyakij <96523298+zhunismp@users.noreply.github.com>
2025-05-12 18:43:52 +0700

    feat: load config from env file (#3)

--- services/product-service/infrastructures/config/utils.go
+++ services/product-service/infrastructures/config/utils.go
@@ -1,21 +1,34 @@
 package config
 
-// TODO: realize this function to load config from environment variables or a config file
+import (
+	"log"
+	"os"
+)
+
 func LoadConfig() Config {
 	appConfig := AppConfig{
-		PORT: "8080",
+		PORT: getEnv("PRODUCT_APP_PORT", "8080"),
 	}
 
 	dbConfig := DBConfig{
-		HOST:     "product-db",
-		PORT:     "5432",
-		USERNAME: "root",
-		PASSWORD: "password",
-		NAME:     "product_db",
+		HOST:     getEnv("PRODUCT_DB_HOST", "product-db"),
+		PORT:     getEnv("PRODUCT_DB_PORT", "5432"),
+		USERNAME: getEnv("PRODUCT_DB_USER", "root"),
+		PASSWORD: getEnv("PRODUCT_DB_PASSWORD", "password"),
+		NAME:     getEnv("PRODUCT_DB_NAME", "product_db"),
 	}
 
 	return Config{
 		APP_CONFIG: appConfig,
 		DB_CONFIG:  dbConfig,
 	}
+}
+
+func getEnv(key, fallback string) string {
+	if val, ok := os.LookupEnv(key); ok {
+		return val
+	}
+
+	log.Default().Println("Using fallback for", key, ": ", fallback)
+	return fallback
 }
