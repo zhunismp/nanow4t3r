8c828ffa20589f4daa9eda57788a64005f8a4bf4
zhunismp <kobkit.zhun@gmail.com>
2025-05-15 09:30:22 +0700

    fix: corrected job dependency

--- services/product-service/core/domain/product.go
+++ services/product-service/core/domain/product.go
@@ -5,8 +5,8 @@ import "time"
 type BottledWater struct {
 	ID        int32     `json:"id"`
 	Name      string    `json:"name"`
-	Size      int16     `json:"size"`  // in CC
-	Price     float32   `json:"price"` // in Bath
+	Size      int16     `json:"size"`  // size in CC
+	Price     float32   `json:"price"` // currency in Bath
 	IsActive  bool      `json:"is_active"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
